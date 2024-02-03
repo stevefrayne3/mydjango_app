a744e8e8d63e7c2a14097eb7b93bf9c86191b9df
stevefrayne3 <stevefrayne3@gmail.com>
2024-02-03 22:44:03 +0530

    updated settings

--- mydjango_app/settings.py
+++ mydjango_app/settings.py
@@ -77,10 +77,10 @@ DATABASES = {
     "default": {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
-        "ENGINE": "django.db.backends.mysql",
+        "ENGINE": "mysql.connector.django",
         "NAME": "aws",
         "USER": "steve",
-        "Password": "steve",
+        "PASSWORD": "steve",
         "HOST": "localhost",
         "PORT": "3306",
     }
